980ead7198cf113742693e9ef5f7e468ea241e81
Matthias Linford <nate.holaday@gmail.com>
2023-07-09 02:52:18 +0000

    now it's a server using the axum crate. Serves an html ascii cat page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
-use pretty_hex::PrettyHex;
+use axum::{
+    body::BoxBody,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+
+use reqwest::StatusCode;
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    println!("{:?}", &image_bytes[..200].hex_dump());
+    let app = Router::new().route("/", get(root_get));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn root_get() -> Response<BoxBody> {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+        Err(e) => {
+            println!("Something went wrong: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+        }
+    }
+}
+
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
     #[derive(Deserialize)]
     struct CatImage {
         url: String,
@@ -25,12 +52,21 @@ async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
 
-    Ok(client
+    let image_bytes = client
         .get(image.url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
-        .await?
-        .to_vec())
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(
+        image,
+        artem::options::OptionBuilder::new()
+            .target(artem::options::TargetType::HtmlFile(true, true))
+            .build(),
+    );
+
+    Ok(ascii_art)
 }
